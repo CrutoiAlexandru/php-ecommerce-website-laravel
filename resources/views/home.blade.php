17b6ccd19fdecfa7c231a66ed3881830dccfe730
Alexandru-Vitali.Crutoi.76010082 <Alexandru-Vitali.Crutoi@microfocus.com>
2023-01-04 19:43:05 +0200

    create auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,76 +1,23 @@
-<?php
-session_start();
-?>
+@extends('layouts.app')
 
-<!DOCTYPE html>
-<html lang="en">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="/global/style.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link href='https://css.gg/shopping-cart.css' rel='stylesheet'>
-</head>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-<body>
-    <div>
-        <h2>
-            <a href="/home">
-                <span>
-                    Pitshop
-                </span>
-            </a>
-        </h2>
-        <div>
-            <a href="/aboutus">
-                <h2>About us</h2>
-            </a>
-            <a href="/cart">
-                <i class="gg-shopping-cart">
-                </i>
-            </a>
-
-            <a href="/profile">
-                <h2>Profile</h2>
-            </a>
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
         </div>
     </div>
-
-    <div style="width: 100vw; display:flex; justify-content: center;">
-        <div class="line"></div>
-    </div>
-
-    <?php
-    if (!array_key_exists('token', $_SESSION)) {
-    ?>
-    <div class="grid">
-        <h1>In order to see what we have in store for you, please log in</h1>
-    </div>
-    <?php
-        return;
-    } ?>
-
-    <div class="grid">
-        <div class="grid-categories">
-            <a href="/home/beanies" class="grid-categories-link" style="color: white;">
-                <h1>Beanies</h1>
-            </a>
-        </div>
-        <div class="grid-categories">
-            <a href="/home/hoodies" class="grid-categories-link" style="color: white;">
-                <h1>Hoodies</h1>
-            </a>
-        </div>
-        <div class="grid-categories">
-            <a href="/home/socks" class="grid-categories-link" style="color: white;">
-                <h1>Socks</h1>
-            </a>
-        </div>
-    </div>
-</body>
-
-</html>
+</div>
+@endsection
